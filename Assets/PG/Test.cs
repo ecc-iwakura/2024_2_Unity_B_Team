4c7c7faccaa3038a89c2ef4973dd34718c25e5db
竹内 <2237032@ecc.ac.jp>
2024-06-03 13:40:38 +0900

    やー

--- Assets/PG/Test.cs
+++ Assets/PG/Test.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+    public GameObject hitBox;
+    public Transform FirePosition;
+
+    float etime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        etime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        etime += Time.deltaTime;
+
+        if (etime > 0.25f)
+        {
+            Instantiate(Bullet, FirePosition.position, FirePosition.rotation);
+            Instantiate(Bullet, FirePosition2.position, FirePosition2.rotation);
+            Instantiate(Bullet, FirePosition3.position, FirePosition3.rotation);
+            etime = 0;
+        }
     }
 }
