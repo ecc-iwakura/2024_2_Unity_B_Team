2dc6677305e0f97a6ecf90dec4f59f362fd0d1a5
竹内 <2237032@ecc.ac.jp>
2024-06-11 13:53:17 +0900

    実機のアニメーション

---
+++ Assets/PG/zikki_anime.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zikki_anime : MonoBehaviour
+{
+    //===== 定義領域 =====
+    private Animator anime;  //Animatorをanimという変数で定義する
+
+    //===== 初期処理 =====
+    void Start()
+    {
+        //変数animに、Animatorコンポーネントを設定する
+        anime = GetComponent<Animator>();
+    }
+
+    //===== 主処理 =====
+    void Update()
+    {
+        //もし、スペースキーが押されたらなら
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anime.SetBool("kougeki", true);
+        }
+    }
+    void Func1()
+    {
+        anime.SetBool("kougeki", false);
+    }
+}
