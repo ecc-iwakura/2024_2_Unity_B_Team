e9c0dd478e9ace114d6737b69f0b4e8993c00a58
竹内 <2237032@ecc.ac.jp>
2024-07-01 11:03:32 +0900

    no message

---
+++ Assets/PG/PlayerController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Weapon
+{
+    public GameObject prefab;
+    public string tag;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform WeaponPositions;
+    private Weapon weapon; // 武器のインスタンスを配列で管理
+    public Weapon[] weaponDataes = new Weapon[3]; // 武器のタグを配列で管理 武器のプレハブ
+    private int currentWeaponIndex = 0; // 現在の武器のインデックス
+
+    private int idx;
+    public GameObject[] PlayerPositions = new GameObject[3];
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        SwitchWeapon(0);
+
+        idx = 1;
+        MoveByIndex(idx);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1～3のキーを押したら対応する武器を切り替える
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1) && weaponDataes.Length >= 1) SwitchWeapon(0);
+
+            if (Input.GetKeyDown(KeyCode.Alpha2) && weaponDataes.Length >= 2) SwitchWeapon(1);
+
+            if (Input.GetKeyDown(KeyCode.Alpha3) && weaponDataes.Length >= 3) SwitchWeapon(2);
+
+            // スペースキーを押したら選択している武器を生成する
+            if (Input.GetKeyDown(KeyCode.Space)) GenerateCurrentWeapon();
+        }
+
+        //プレイヤーの移動処理
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (idx > 0)
+                {
+                    idx--;
+                    MoveByIndex(idx);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (idx < 2)
+                {
+                    idx++;
+                    MoveByIndex(idx);
+                }
+            }
+        }
+    }
+    private void SwitchWeapon(int index)
+    {
+        // 指定された武器をアクティブにする
+        weapon = weaponDataes[index];
+        // 現在の武器のインデックスを更新
+        currentWeaponIndex = index;
+    }
+    private void GenerateCurrentWeapon()
+    {
+        // 現在選択されている武器を生成する
+        if (weapon!= null)
+        {
+            GameObject obj=Instantiate(weapon.prefab, WeaponPositions.position, Quaternion.identity);
+            obj.tag = weapon.tag;
+        }
+    }
+    void DestroyWithTag(string tagToDestroy)
+    {
+        GameObject[] objectsToDestroy = GameObject.FindGameObjectsWithTag(tagToDestroy);
+
+        foreach (GameObject obj in objectsToDestroy) Destroy(obj); 
+    }
+
+    void MoveByIndex(int index)
+    {
+        transform.position = PlayerPositions[index].transform.position;
+    }
+}
--- Assets/PG/RandomSpwn.cs
+++ Assets/PG/RandomSpwn.cs
@@ -4,21 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class RandomSpwn : MonoBehaviour
 {
-    public GameObject Enemy;
-    public GameObject Enemy2;
-    public GameObject Enemy3;
-    public GameObject Enemy4;
-    public GameObject Enemy5;
-    public GameObject Enemy6;
+    public GameObject Enemy1, Enemy2, Enemy3, Enemy4, Enemy5, Enemy6;
     public Transform[] spawnPoints; // 生成場所の配列
 
     private int r;
     private int spawnIndex;
-    public float waitTime = 1.0f;
 
+
+    public float waitTime = 1.0f;//生成のディレイ
+    public float ShortenTime = 1.0f;//waitTimeを短縮する
+    public float minimum =1.0f;//生成間隔の最小値
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(StartSpawningWithDelay(3.0f)); // 3秒の遅延を追加
+    }
+
+    private IEnumerator StartSpawningWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // 指定した秒数待機
         StartCoroutine(Spawn());
     }
 
@@ -26,37 +30,42 @@ public class RandomSpwn : MonoBehaviour
     {
         while (true)
         {
-            r = Random.Range(1, 7); // 1か3までのランダムな整数を生成
+            r = Random.Range(1, 7); // 1から6までのランダムな整数を生成
             spawnIndex = Random.Range(0, spawnPoints.Length); // 生成場所のランダムなインデックスを生成
 
             switch (r)
             {
                 case 1:
-                    Instantiate(Enemy, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
-
+                    Instantiate(Enemy1, spawnPoints[spawnIndex].position,
+                                        spawnPoints[spawnIndex].rotation);
                     break;
                 case 2:
-                    Instantiate(Enemy2, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
-
+                    Instantiate(Enemy2, spawnPoints[spawnIndex].position,
+                                        spawnPoints[spawnIndex].rotation);
                     break;
                 case 3:
-                    Instantiate(Enemy3, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation); // 新しいゲームオブジェクトを生成
-
+                    Instantiate(Enemy3, spawnPoints[spawnIndex].position,
+                                        spawnPoints[spawnIndex].rotation);
                     break;
                 case 4:
-                    Instantiate(Enemy4, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation); // 新しいゲームオブジェクトを生成
-
+                    Instantiate(Enemy4, spawnPoints[spawnIndex].position,
+                                        spawnPoints[spawnIndex].rotation);
                     break;
                 case 5:
-                    Instantiate(Enemy5, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation); // 新しいゲームオブジェクトを生成
-
+                    Instantiate(Enemy5, spawnPoints[spawnIndex].position,
+                                        spawnPoints[spawnIndex].rotation);
                     break;
                 case 6:
-                    Instantiate(Enemy6, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation); // 新しいゲームオブジェクトを生成
-
+                    Instantiate(Enemy6, spawnPoints[spawnIndex].position,
+                                        spawnPoints[spawnIndex].rotation);
                     break;
             }
-            yield return new WaitForSeconds(waitTime); // x秒待機
+
+            waitTime -= ShortenTime; // waitTimeだんだんを短縮する
+
+            if (waitTime < minimum) waitTime = minimum;
+
+            yield return new WaitForSeconds(waitTime);//生成のディレイ
         }
     }
 }
