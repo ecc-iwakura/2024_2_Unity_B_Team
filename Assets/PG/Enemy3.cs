e4326c1a94f734e537804ddd565bc5b9d961966d
吉田 <2237014@ecc.ac.jp>
2024-07-29 12:56:42 +0900

    いろいろ

--- Assets/PG/Enemy3.cs
+++ Assets/PG/Enemy3.cs
@@ -8,6 +8,7 @@ public class Enemy3 : MonoBehaviour
     private ScoreManager score;
     public static bool des = false;
     public GameObject ObjectZan;
+    public GameObject hitPrefab;
     private AudioSource sound1;
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,17 @@ public class Enemy3 : MonoBehaviour
         {
             score.AddScore(1);
             Destroy(gameObject);
+            Destroy(collision.gameObject);
+            if (sound1 != null && sound1.clip != null)
+            {
+                sound1.PlayOneShot(sound1.clip); // 音声を再生する
+            }
+
+            // Hit プレファブを表示させる
+            if (hitPrefab != null)
+            {
+                Instantiate(hitPrefab, collision.transform.position, Quaternion.identity);
+            }
             if (ObjectZan != null)
             {
                 Instantiate(ObjectZan, transform.position, transform.rotation);
